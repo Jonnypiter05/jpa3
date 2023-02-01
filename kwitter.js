c245600c10442bed8d12dd3c9f736944cd6171e9
Jonnypiter05 <111024941+Jonnypiter05@users.noreply.github.com>
2023-02-01 19:03:07 -0300

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,23 @@
+
+function addUser() {
+
+    userName = document.getElementById("userName").value;
+  
+    localStorage.setItem("userName", userName);
+    
+      window.location = "kwitter_room.html";
+  }
+  
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyBKa6GMnoxgKDxZzLdkmcftgA5PqUslP7U",
+    authDomain: "projeto-do-conversar-40095.firebaseapp.com",
+    databaseURL: "https://projeto-do-conversar-40095-default-rtdb.firebaseio.com",
+    projectId: "projeto-do-conversar-40095",
+    storageBucket: "projeto-do-conversar-40095.appspot.com",
+    messagingSenderId: "149000015194",
+    appId: "1:149000015194:web:2f9f929a17ff2a9573b763"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
